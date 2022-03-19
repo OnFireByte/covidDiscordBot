158f6663affa1ba9391021e095674f0bf69a9b00
Pakin Puttanukulchai <65996113+OnFireByte@users.noreply.github.com>
2022-03-19 12:19:26 +0700

    Update dailyFetch.js

--- module/dailyFetch.js
+++ module/dailyFetch.js
@@ -1,4 +1,4 @@
-import { readFile } from "fs";
+import { readFile } from "fs/promises";
 import { DateTime } from "luxon";
 import { fetchAPI } from "./fetchAndUpdate.js";
 import { messageToChannels } from "./messageToChannels.js";
@@ -6,8 +6,8 @@ import { messageToChannels } from "./messageToChannels.js";
 export const dailyFetch = async (tryCount = 0) => {
     const today = DateTime.now().setZone("Asia/Bangkok").day;
     await fetchAPI();
-    readFile("./Data/data.json", "utf8", async (_err, data) => {
-        const cacheData = JSON.parse(data);
+    await readFile("./Data/data.json", "utf8", async (_err, data) => {
+        const cacheData = await JSON.parse(data);
         const newestData = cacheData[cacheData.length - 1];
         const newestDate = Number(newestData.txn_date.split("-")[2]);
         if (newestDate == today || tryCount >= 5) {
