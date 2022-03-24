484e0a9990f460a0d8bc4e9ab1e9ce48961af441
Pakin Puttanukulchai <65996113+OnFireByte@users.noreply.github.com>
2022-03-24 11:37:22 +0700

    extract callback of readFile in dailyFetch

--- module/dailyFetch.js
+++ module/dailyFetch.js
@@ -7,17 +7,16 @@ export const dailyFetch = async (tryCount = 0) => {
     console.log("On dailyFetch function");
     const today = DateTime.now().setZone("Asia/Bangkok").day;
     await fetchAPI();
-    await readFile("./Data/data.json", "utf8", async (_err, data) => {
-        const cacheData = await JSON.parse(data);
-        const newestData = cacheData[cacheData.length - 1];
-        const newestDate = Number(newestData.txn_date.split("-")[2]);
-        if (newestDate == today || tryCount >= 5) {
-            messageToChannels();
-            return;
-        }
-        console.log("The data is not up-to-date, will fetch data again in next 1 hour");
-        setTimeout(async () => {
-            await dailyFetch(tryCount + 1);
-        }, 3600000);
-    });
+    const data = await readFile("./Data/data.json", "utf8");
+    const cacheData = await JSON.parse(data);
+    const newestData = cacheData[cacheData.length - 1];
+    const newestDate = Number(newestData.txn_date.split("-")[2]);
+    if (newestDate == today || tryCount >= 5) {
+        messageToChannels();
+        return;
+    }
+    console.log("The data is not up-to-date, will fetch data again in next 1 hour");
+    setTimeout(async () => {
+        await dailyFetch(tryCount + 1);
+    }, 3600000);
 };
